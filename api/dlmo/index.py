725a9b7b9525ff0cfecc33c83d2f6808eeb29775
halakkitach <99856703+halakkitach@users.noreply.github.com>
2023-05-17 14:47:52 +0700

    Update index.py

--- api/dlmo/index.py
+++ api/dlmo/index.py
@@ -39,6 +39,17 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
+with open('../dlmo/aanteevee_info.txt') as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        if not line.startswith('https:'):
+            line = line.split('|')
+            ch_name = line[0].strip()
+            grp_title = line[1].strip().title()
+            tvg_logo = line[2].strip()
+            tvg_id = line[3].strip()
         else:
             grab(line)
             
