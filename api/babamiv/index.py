04400643b6f73fe7ed4be042bef2f9c8e059de7c
halakkitach <99856703+halakkitach@users.noreply.github.com>
2023-05-25 10:50:47 +0700

    Update index.py

--- api/babamiv/index.py
+++ api/babamiv/index.py
@@ -30,5 +30,5 @@ def grab(url):
         return response
 
 s = requests.Session()
-result = grab('https://testingvidm3.herokuapp.com/index.php?id=' + str(sys.argv[1]))
+result = grab('https://vidiohls.combipart1.workers.dev/index.m3u8?id=' + str(sys.argv[1]))
 print(result)
